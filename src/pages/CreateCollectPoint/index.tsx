6814a99f2d5eebf282e2aa8b643a57b56fde3a01
Matheus Felipe Araújo <matheus151929@live.com>
2020-06-13 10:26:02 -0300

    api cep

--- src/pages/CreateCollectPoint/index.tsx
+++ src/pages/CreateCollectPoint/index.tsx
@@ -55,50 +55,50 @@ const CreateCollectPoint = () => {
 
     const history = useHistory();
 
-    useEffect(() => 
+    useEffect(() =>
     {
 
         api.get<CollectItem[]>('collect_items')
-        .then((response) => 
+        .then((response) =>
         {
             console.log(response.data)
-          if (response.status === 200) 
+          if (response.status === 200)
           {
             setCollectItems(response.data)
-          }      
+          }
         })
         .catch(err => console.error(err));
-        
+
         axios.get<Estado[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
-        .then((response) => 
+        .then((response) =>
         {
-          if (response.status === 200) 
+          if (response.status === 200)
           {
             setEstados(response.data)
-          }      
+          }
         })
 
         .catch(err => console.error(err));
 
-        navigator.geolocation.getCurrentPosition(position => 
+        navigator.geolocation.getCurrentPosition(position =>
             {
-                setInitialPosition([position.coords.latitude, position.coords.longitude])              
+                setInitialPosition([position.coords.latitude, position.coords.longitude])
             }
         )
 
     }, [])
 
-    useEffect(() => 
+    useEffect(() =>
     {
         setLoadingCity(true);
 
         axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estado}/municipios`)
-        .then((response) => 
+        .then((response) =>
         {
-          if (response.status === 200) 
+          if (response.status === 200)
           {
             setCitys(response.data)
-          }      
+          }
           setLoadingCity(false);
         })
         .catch(err => {
@@ -115,7 +115,7 @@ const CreateCollectPoint = () => {
         if (!value) {
             return;
         }
-        
+
         const alreadySelected = selectedCollectItem.includes(value)
         const newSelectedItems = [...selectedCollectItem];
 
@@ -154,14 +154,39 @@ const CreateCollectPoint = () => {
         setSelectedPosition([latlng.lat, latlng.lng]);
     }
 
-    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => 
+    function handleCEP(cep: string) {
+        if (cep.length < 8) {
+            return;
+        }
+            axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+                .then((response) =>
+                {
+                    if (response.status === 200)
+                    {
+                        const { logradouro, bairro, localidade, uf } = response.data;
+                        setFormaData({...formData, bairro, rua: logradouro})
+                        setCity(localidade);
+                        setEstado(uf);
+                    }
+                })
+                .catch(err => {
+                    console.error(err)
+                })
+                .finally(() => setLoadingCity(false));
+    }
+
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) =>
     { const {value, name} = event.target;
 
       if(!value || !name) {
           return;
-      } 
+      }
 
-      console.log(value, name)
+      if (value === 'zip-code' || name === 'zip-code')
+      {
+            handleCEP(value.replace(/[^0-9]/g, ''));
+
+      }
 
       setFormaData({...formData, [name]: value})
 
@@ -174,13 +199,9 @@ const CreateCollectPoint = () => {
 
 
         const data = new FormData();
- 
 
         const {name, email, rua, bairro, numero, telephone, cellphone} = formData;
         const [latitude, longitude] = selectedPosition;
-
-        console.log(formData, estado, city)
-        console.log(latitude, longitude, selectedCollectItem, telephone);
 
         if (!selectedCollectItem.length) {
             alert('Pelo menos um item de coleta deve ser selecionado');
@@ -188,7 +209,6 @@ const CreateCollectPoint = () => {
             return;
         }
 
-    
         data.append('name',name);
         data.append('email',email);
         data.append('UF', estado);
@@ -206,23 +226,23 @@ const CreateCollectPoint = () => {
         if (selectedFile) {
             data.append('image', selectedFile);
         }
-        
+
 
         api.post('collect_points', data)
-        .then((response) => 
+        .then((response) =>
         {   console.log(response)
             if (response.status === 200) {
                 alert('Ponto de coleta cadastrado com sucesso!');
                 history.push('/');
             }
-            setLoadingForm(false);  
+            setLoadingForm(false);
         })
         .catch(err => {
             console.error(err);
-            setLoadingForm(false);  
+            setLoadingForm(false);
             alert('Erro ao cadastrar o ponto de coleta!');
         });
-        
+
     }
 
     return (
@@ -236,12 +256,12 @@ const CreateCollectPoint = () => {
                 <h1>Cadastro do <br /> ponto de coleta</h1>
 
                 <FileDropzone FileDropzone={setSelectedFile}/>
-        
+
                 <fieldset>
                     <legend> <h2>Dados </h2> </legend>
                     <div className="field">
                         <label htmlFor="name">Nome da entidade*</label>
-                        <input 
+                        <input
                             type="text"
                             name="name"
                             id="name"
@@ -252,7 +272,7 @@ const CreateCollectPoint = () => {
 
                     <div className="field">
                             <label htmlFor="email">E-mail*</label>
-                            <input 
+                            <input
                                 type="email"
                                 name="email"
                                 id="email"
@@ -263,11 +283,11 @@ const CreateCollectPoint = () => {
                         </div>
 
                     <div className="field-group">
-                     
+
 
                        <div className="field">
                         <label htmlFor="telephone">Telefone*</label>
-                            <input 
+                            <input
                                 type="text"
                                 name="telephone"
                                 id="telephone"
@@ -278,8 +298,8 @@ const CreateCollectPoint = () => {
                         </div>
 
                        <div className="field">
-                        <label htmlFor="cellphone">Celular</label>
-                            <input 
+                        <label htmlFor="cellphone">Celular*</label>
+                            <input
                                 type="text"
                                 name="cellphone"
                                 id="cellphone"
@@ -292,8 +312,8 @@ const CreateCollectPoint = () => {
                 </fieldset>
 
                 <fieldset>
-                    <legend> 
-                        <h2>Endereço </h2> 
+                    <legend>
+                        <h2>Endereço </h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
 
@@ -303,30 +323,32 @@ const CreateCollectPoint = () => {
                         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
-                        <Marker position={selectedPosition[0] || selectedPosition[1] 
-                            ? selectedPosition 
-                            : initialPosition} 
+                        <Marker position={selectedPosition[0] || selectedPosition[1]
+                            ? selectedPosition
+                            : initialPosition}
                         />
                     </Map>
 
                     <div className="field-group">
                         <div className="field">
                              <label htmlFor="zil-code">CEP*</label>
-                                <input 
+                                <input
                                     type="text"
                                     name="zip-code"
                                     id="zip-code"
+                                    value={ formData['zip-code'].replace(/\D/g, '').replace(/^(\d{5})(\d)/, '$1-$2')}
                                     required
                                     onChange={handleInputChange}
                                 />
                         </div>
 
                         <div className="field">
-                             <label htmlFor="bairro">Bairro</label>
-                                <input 
+                             <label htmlFor="bairro">Bairro*</label>
+                                <input
                                     type="text"
                                     name="bairro"
                                     id="bairro"
+                                    value={formData.bairro}
                                     required
                                     onChange={handleInputChange}
                                 />
@@ -334,21 +356,22 @@ const CreateCollectPoint = () => {
                     </div>
 
                     <div className="field-group">
-                       
+
                     <div className="field">
-                                <label htmlFor="rua">Rua</label>
-                                    <input 
+                                <label htmlFor="rua">Rua*</label>
+                                    <input
                                         type="text"
                                         name="rua"
                                         id="rua"
+                                        value={formData.rua}
                                         required
                                         onChange={handleInputChange}
                                     />
                             </div>
 
                         <div className="field">
-                             <label htmlFor="numero">Número</label>
-                                <input 
+                             <label htmlFor="numero">Número*</label>
+                                <input
                                     type="number"
                                     name="numero"
                                     id="numero"
@@ -359,49 +382,51 @@ const CreateCollectPoint = () => {
                         </div>
                     </div>
 
-                    <div className="field">
-                        <label htmlFor="uf">Estado</label>
-                        <select name="uf" id="uf" onChange={handleSelectedState} required>
-                            <option value="0">Selecione um estado*</option>
-                            {
-                                estados.map(({nome, sigla, id}) => (<option key={id} value={sigla}>{`${sigla} - ${nome}`}</option>))
-                            }
-                        </select>
-                                 
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="uf">Estado*</label>
+                            <select name="uf" id="uf" value={estado} onChange={handleSelectedState} required>
+                                <option value="0">Selecione um estado*</option>
+                                {
+                                    estados.map(({nome, sigla, id}) => (<option key={id} value={sigla}>{`${sigla} - ${nome}`}</option>))
+                                }
+                            </select>
+
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="city">Cidade*</label>
+                            <select name="city" id="city" value={city} onChange={handleSelectedState} required>
+                                <option value="0">
+                                    {!loadingCity
+                                        ? estado && estado.length
+                                            ? `Selecione uma cidade do ${estado}`
+                                            : 'Selecione uma cidade*'
+                                        : `Carregando cidades do ${estado}`
+                                    }
+                                </option>
+                                {
+                                    citys.map(({nome, id}) => (<option key={id} value={nome}>{nome}</option>))
+                                }
+                            </select>
+                        </div>
                     </div>
-                    
-                    <div className="field">
-                            <label htmlFor="city">Cidade</label>
-                            <select name="city" id="city" onChange={handleSelectedState} required>
-                            <option value="0">
-                           {!loadingCity 
-                             ? estado.length 
-                                ? `Selecione uma cidade do ${estado}`
-                                 : 'Selecione uma cidade*'
-                             : `Carregando cidades do ${estado}`
-                            }
-                         </option>
-                            {
-                                citys.map(({nome, id}) => (<option key={id} value={nome}>{nome}</option>))
-                            }
-                        </select>
-                    </div>
-                    
+
                 </fieldset>
                 <fieldset>
-                    <legend> 
-                        <h2>Itens de Coleta </h2> 
+                    <legend>
+                        <h2>Itens de Coleta </h2>
                         <span>Selecione um ou mais itens abaixo*</span>
                     </legend>
-                  
+
                   <ul className="items-grid" onChange={handleSelectedItem}>
                       {
-                          collectItems.map(({image_url, title, id}) => 
+                          collectItems.map(({image_url, title, id}) =>
                           (
-                                <li 
-                                    key={id} 
-                                    className={selectedCollectItem.includes(id) ?  'selected' : ''} 
-                                    value={id} 
+                                <li
+                                    key={id}
+                                    className={selectedCollectItem.includes(id) ?  'selected' : ''}
+                                    value={id}
                                     onClick={handleSelectedItem}
                                 >
                                     <img id={id.toString()} src={image_url} alt="item de coleta"/>
@@ -411,7 +436,7 @@ const CreateCollectPoint = () => {
                         )
                       }
                   </ul>
-                    
+
                 </fieldset>
 
                 <button type="submit"> {loadingForm ? 'Cadastrando...' : 'Cadastrar ponto de coleta'}</button>
